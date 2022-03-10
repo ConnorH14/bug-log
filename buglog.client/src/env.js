aeab7b890561fa2402b1d6f3b192d2a0e820ec8e
ConnorH14 <ConnorH14@live.com>
2022-03-10 02:18:24 -0700

    test

--- buglog.client/src/env.js
+++ buglog.client/src/env.js
@@ -3,3 +3,5 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'chines.us.auth0.com'
 export const audience = 'https://buglog.com'
 export const clientId = 'RRIOIDvtPhqPhDdAs0MQ3Kxa4KL5ZB5G'
+
+// test
